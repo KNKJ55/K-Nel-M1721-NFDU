df562ce113f6225659e8b9f490279e2b4f54de83
Roman Rihter <teledurak@gmail.com>
2021-11-19 19:23:05 +0300

    m1721: mido -> m1721
    Change-Id: Ie1a6617704b816cb7d8e2abe5eae6521d3d698f8

--- techpack/audio/dsp/q6voice.c
+++ techpack/audio/dsp/q6voice.c
@@ -4365,7 +4365,7 @@ static int voice_get_avcs_version_per_service(uint32_t service_id)
 		return -EINVAL;
 	}
 
-#ifdef CONFIG_MACH_XIAOMI_C6
+#ifdef CONFIG_MACH_MEIZU_M1721
 	common.is_avcs_version_queried = true;
 	return CVP_VERSION_1;
 #endif
